e4a95f43bd4e63bab08bb25f1f93e73e19bc7919
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-13 20:13:37 +0100

    created side menu for mobile

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 function show_menu() {
-    $('#mobile-overlay').show();    
+    $('#mobile-overlay').show();
 }
 function hide_menu() {
     $('#mobile-overlay').hide();    
 }
 function isMobile() {
-    return $(window).width <= 400 ;
+    return $(window).width() <= 400 ;
 }
 
 function make_side_panel() {
-    
+    $('#mobile-menu-container').append($('#site-nav'));    
+    $('#mobile-menu-container').append($('#side-bar'));    
 }
+function set_menu_button_position() {
 
+}
+function set_page_container_padding_top() {
+
+}
 function toggle_sidebar() {
     if (isMobile()) {
         make_side_panel();
     }
+    set_menu_button_position();
+    set_page_container_padding_top();
 }
 
 
