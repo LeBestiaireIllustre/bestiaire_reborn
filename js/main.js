1d22a3d098f9b7ac08b0e588926defb3e786cd88
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-14 13:09:07 +0100

    Fixed mobile menu button positionning

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
+var mobileMenuWidth = '15.5em';
 function show_menu() {
     $('#mobile-overlay').show();
+    $('#mobile-menu-container').animate({
+        'right': '0em'
+    }, 400);
+    $('html,body').addClass('noscroll');
 }
 function hide_menu() {
-    $('#mobile-overlay').hide();    
+    $('#mobile-overlay').hide();
+    $('#mobile-menu-container').animate({
+        'right': '-' + mobileMenuWidth
+    }, 400);
+    $('html,body').removeClass('noscroll');
 }
 function isMobile() {
-    return $(window).width() <= 400 ;
+    return $(window).width() <= 640 ;
 }
 
 function make_side_panel() {
     $('#mobile-menu-container').append($('#site-nav'));    
     $('#mobile-menu-container').append($('#side-bar'));    
 }
-function set_menu_button_position() {
-
-}
-function set_page_container_padding_top() {
-
-}
 function toggle_sidebar() {
     if (isMobile()) {
         make_side_panel();
     }
-    set_menu_button_position();
-    set_page_container_padding_top();
 }
 
 
@@ -35,5 +36,6 @@ $(document).ready(function () {
     $('#mobile-overlay').click(function() {
         hide_menu();
     });
+    
     toggle_sidebar();
 });
