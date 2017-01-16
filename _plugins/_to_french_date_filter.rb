3ff42f8a427a87f61740ccf4e7c64e0496bf3339
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-16 20:57:02 +0100

    created archives view

---
+++ _plugins/_to_french_date_filter.rb
@@ -0,0 +1,14 @@
+module Jekyll
+    module FrenchDateFilter
+        def to_french_date(date)
+            puts 'kablam'
+            months = ['Janvier','Février','Mars','Avril','Mai','Juin',
+            'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+            elems = date.split('-')
+            month = Integer(elems[1])
+            elems[1] = months[month-1]
+            return elems.join('&nbsp;')
+        end
+    end
+end
+Liquid::Template.register_filter(Jekyll::FrenchDateFilter)
