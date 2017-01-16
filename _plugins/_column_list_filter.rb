ce608c4e5cfb743438ad7246a3aee61ab814c323
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-01-16 14:04:18 +0100

    fixed bestiaire hovering text left/right positionning

--- _plugins/_column_list_filter.rb
+++ _plugins/_column_list_filter.rb
@@ -1,16 +1,36 @@
 module Jekyll
     module ColumnListFilter
+        
+        class Column
+            attr_reader :images, :positioning
+            def initialize(images, positioning)
+                @images = images
+                @positioning = positioning
+            end
+
+            def to_liquid(*args) 
+                {
+                    'images' => @images,
+                    'positioning' => @positioning
+                }
+            end
+        end
+        
         def column_list(flatList, columnsCount)
             columnsCount = Integer(columnsCount)
             result = []
             for i in (0...columnsCount)
-                result.push([])
+                if (i < (columnsCount / 2))
+                    result.push(Column.new([], 'left'))
+                else 
+                    result.push(Column.new([], 'right'))
+                end
             end
 
             for i in (0...flatList.length).step(columnsCount)
                 for j in (0...columnsCount)
                     unless (flatList[i+j].nil?)
-                        result[j].push(flatList[i+j])
+                        result[j].images.push(flatList[i+j])
                     end
                 end
             end
