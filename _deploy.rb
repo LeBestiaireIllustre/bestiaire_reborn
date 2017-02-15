53ffd64cd5463b770ec65dd790ae6019db5b1d23
Chedy Missaoui <missaoui.chedy@gmail.com>
2017-02-15 16:37:51 +0100

    Made the deploy script figure out its directory by itself

--- _deploy.rb
+++ _deploy.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/ruby
 require 'fileutils'
 BASE_URL = 'http://www.lebestiaireillustre.com'
-BASE_PATH = '~/bestiaire_reborn'
+BASE_PATH = File.expand_path(File.dirname(__FILE__))
+#BASE_PATH = '/home/chedy/blog_adel/bestiaire_reborn'
 def abs_path(path)
     return "#{BASE_PATH}/#{path}"
 end
